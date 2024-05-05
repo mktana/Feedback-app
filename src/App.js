e64304380548747bbfce9043e56b7a748bb44ddb
moriya <moriyakatan@gmail.com>
2024-05-05 15:15:35 +0300

    set old school condition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 function App () {
     const title = 'Blog Post';
-    
+    const body = 'This is a blog post'
+    const comments = [
+        {id:1, text: 'Comment one'},
+        {id:2, text: 'Comment two'},
+        {id:3, text: 'Comment three'}
+    ]
+
+    const loading = false;
+    const showComments = true;
+
+    if(!loading) return <h1>Loading...</h1>
 
     return (
         <div className='container'>
-        <h1>My App</h1>
+        <h1>{title.toUpperCase()}</h1>
+        <p>{body}</p>
+
+        {showComments ? 'yes' : 'no'}        
+
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
         </div>
     )
 }
