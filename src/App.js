64a29e17d6727ea6f68a09fefa61abaa8b4c6db5
moriya <moriyakatan@gmail.com>
2024-05-20 12:45:56 +0300

    homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import {v4 as uuidv4} from 'uuid';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+//import AboutPage from './pages/AboutPage';
+
 import FeedbackData from './data/FeedbackData';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if(window.confirm('Are you sure you want to delete this feedback')) {
@@ -14,15 +24,19 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className='container'>
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback}
         handleDelete={deleteFeedback} />
+        {/* <AboutPage /> */}
+        <Route path='/about' Component={AboutPage} />
       </div>
-    </>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/components/RatingSelect.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+function RatingSelect({select}) {
+  const [selected, setSelected] = useState(10);
+
+  const handleChange = (e) => {
+    setSelected(+e.currentTarget.value);
+    select(selected)
+  };
+
+  return (
+    <ul className='rating'>
+      <li>
+        <input
+          type='radio'
+          id='num1'
+          name='rating'
+          value='1'
+          onChange={handleChange}
+          checked={selected === 1}
+        />
+        <label htmlFor="num1">1</label>
+      </li>
+    </ul>
+    //     <ul className='rating'>
+    //     {Array.from({ length: 10 }, (_, i) => (
+    //       <li key={`rating-${i + 1}`}>
+    //         <input
+    //           type='radio'
+    //           id={`num${i + 1}`}
+    //           name='rating'
+    //           value={i + 1}
+    //           onChange={handleChange}
+    //           checked={selected === i + 1}
+    //         />
+    //         <label htmlFor={`num${i + 1}`}>{i + 1}</label>
+    //       </li>
+    //     ))}
+    //   </ul>
+  );
+}
+
+export default RatingSelect;
